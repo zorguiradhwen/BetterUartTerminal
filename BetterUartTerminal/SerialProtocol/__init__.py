8766daaa07a59c7acf3fc6486a6c2519686b10b6
Radhwen Zorgui <zorgui.radhwen@gmail.com>
2021-01-25 18:05:26 +0100

    restructure project folder based on recommended architectures

---
+++ BetterUartTerminal/SerialProtocol/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 17:56:37 2021
+
+@author: radhwen.zorgui
+"""
+
---
+++ BetterUartTerminal/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 17:53:52 2021
+
+@author: radhwen.zorgui
+"""
+
